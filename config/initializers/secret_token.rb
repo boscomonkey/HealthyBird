a657e6d08326759a18036f60b825fb8811e79f57
Bosco So <bosco@sfruby.info>
2014-02-09 15:49:36 -0800

    turning into Rails app for easy deploy to Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealthyBird::Application.config.secret_token = '82e493573d58fc98f205c9289c4c42edac1608ab9a748b42192d9c1ff0e1050668fb6288e3b0847e01e0ef25ba43ac8b7ef9aedb1a1b767f29b64729da631079'
