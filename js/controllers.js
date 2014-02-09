48fb40ce9b9b69858664268100bfbc2b3720c6f7
Brian <bclark8923@gmail.com>
2014-02-08 17:46:00 -0800

    added login and points

--- js/controllers.js
+++ js/controllers.js
@@ -7,27 +7,102 @@
 angular.module('myApp.controllers', [])
     .controller('HomeCtrl', ['$scope', '$location', function($scope, $location) {       
         $scope.signupToday = function() {
-            $location.path('/signup');   
+            $location.path('/signup');  
+            if(!$scope.$$phase) $scope.$apply(); 
         }
     }])
 
-    .controller('SingleObjectiveCtrl', ['$scope', '$rootScope', '$routeParams', function($scope, $rootScope, $routeParams, $location) {
-        //stuff
-        $scope.objectiveName = $routeParams.objectiveId;
+    .controller('SingleObjectiveCtrl', ['$scope', '$rootScope', '$routeParams', '$location', function($scope, $rootScope, $routeParams, $location) {
+        $scope.user = Parse.User.current();
+        if($routeParams.objectiveId == "excercise-more") {
+            $scope.objectiveName = "Excercise!";
+            $scope.objective = "Go do 12 jumping jacks! 20 points";
+            $scope.points = 20;
+        }
+        else if($routeParams.objectiveId == "eat-healthy") {
+            $scope.objectiveName = "Eat Healthy!";
+            $scope.objective = "Go buy Bananas at Whole Foods! 5 points";
+            $scope.points = 5;
+        }
         
+        $scope.completeTask = function() {
+            var points = parseInt(Parse.User.current().get("points"));
+            points += $scope.points;
+            Parse.User.current().set("points", points);
+            Parse.User.current().save(null, {
+              success: function(user) {
+                $location.path("/objectives");
+                if(!$scope.$$phase) $scope.$apply();
+              }
+            });
+        }
     }])
 
     .controller('ObjectivesCtrl', ['$scope', '$rootScope', '$routeParams', function($scope, $rootScope, $location) {
-        //stuff
+        $scope.user = Parse.User.current();
         
     }])
 
     .controller('MainCtrl', ['$scope', '$rootScope', '$routeParams', function($scope, $rootScope, $location) {
-        //stuff
+        $scope.user = Parse.User.current();
         
     }])
 
     .controller('StoreCtrl', ['$scope', '$rootScope', '$routeParams', function($scope, $rootScope, $location) {
-        //stuff
+        $scope.user = Parse.User.current();
         
+    }])
+
+    .controller('AcctCtrl', ['$scope', '$rootScope', '$location', function($scope, $rootScope, $location) {
+        
+        $scope.createAccount = function() {
+            $scope.err = null;
+            if( assertValidLoginAttempt() ) {
+                // create parse account
+                var user = new Parse.User();
+                user.set("username", $scope.username);
+                user.set("password", $scope.pass);
+                user.set("points", 0);
+                
+                user.signUp(null, {
+                  success: function(user) {
+                    $location.path('/main');
+                    if(!$scope.$$phase) $scope.$apply(); 
+                  },
+                  error: function(user, error) {
+                    // Show the error message somewhere and let the user try again.
+                    $scope.err = "Error: " + error.message;
+                  }
+                });
+            }
+        };
+        
+        
+        $scope.login = function(cb) {
+            $scope.err = null;
+            if( assertValidLoginAttempt() ) {
+                Parse.User.logIn($scope.username, $scope.pass, {
+                  success: function(user) {
+                    // Do stuff after successful login.
+                    $rootScope.user = user;
+                    $location.path('/main');
+                    if(!$scope.$$phase) $scope.$apply(); 
+                  },
+                  error: function(user, error) {
+                    // The login failed. Check error to see why.
+                    $scope.err = "Error: " + error.message;
+                  }
+                });
+             }
+        };
+        
+        function assertValidLoginAttempt(isSigningUp) {
+            if( !$scope.username ) {
+                $scope.err = 'Please enter an email address';
+            }
+            else if( !$scope.pass ) {
+                $scope.err = 'Please enter a password';
+            }
+            return !$scope.err;
+        }
     }])
--- js/routes.js
+++ js/routes.js
@@ -35,62 +35,14 @@ angular.module('myApp.routes', ['ngRoute'])
          controller: 'StoreCtrl'
       });
 
-      $routeProvider.when('/chat', {
-         templateUrl: 'partials/chat.html',
-         controller: 'ChatCtrl'
-      });
-
-      $routeProvider.when('/account', {
-         authRequired: true, // must authenticate before viewing this page
-         templateUrl: 'partials/account.html',
-         controller: 'AccountCtrl'
-      });
-
-      $routeProvider.when('/profile/:userId', {
-         templateUrl: 'partials/viewSingleUser.html',
-         controller: 'ViewSingleUserCtrl'
-      });
-
-      $routeProvider.when('/artpieces', {
-         templateUrl: 'partials/viewAllArt.html',
-         controller: 'ViewAllArtCtrl'
-      });
-
-      $routeProvider.when('/artpieces/:artId', {
-         authRequired: true, // must authenticate before viewing this page
-         templateUrl: 'partials/viewSingleArt.html',
-         controller: 'ViewSingleArtCtrl'
-      });
-
-      $routeProvider.when('/orgs', {
-         templateUrl: 'partials/viewAllOrg.html',
-         controller: 'ViewAllOrgCtrl'
-      });
-
-      $routeProvider.when('/orgs/:orgId', {
-         templateUrl: 'partials/viewSingleOrg.html',
-         controller: 'ViewSingleArtsOrgCtrl'
-      });
-
-      $routeProvider.when('/createOrg', {
-         authRequired: true, // must authenticate before viewing this page
-         templateUrl: 'partials/createOrg.html',
-         controller: 'CreateArtOrgCtrl'
-      });
-
-      $routeProvider.when('/createTarget', {
-         templateUrl: 'partials/createTarget.html',
-         controller: 'CreateTargetCtrl'
-      });
-
       $routeProvider.when('/login', {
          templateUrl: 'partials/login.html',
-         controller: 'AuthCtrl'
+         controller: 'AcctCtrl'
       });
 
       $routeProvider.when('/signup', {
          templateUrl: 'partials/signup.html',
-         controller: 'AuthCtrl'
+         controller: 'AcctCtrl'
       });
 
       $routeProvider.otherwise({redirectTo: '/home'});
